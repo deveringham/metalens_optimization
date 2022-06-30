6a9a89008d6b89294a2741317d430dd7ea1d13f3
Dylan Everingham <dceveringham@gmail.com>
2022-06-30 13:39:46 +0200

    First cut at multilayer optimization via heights
    In the new method, shown in metasurface_optimization.ipynb and nearfield_optimization.ipynb, float heights of each pixel are directly optimized, leading to a scalable method for many device layers. The old method, with one layer optimized at a time, is for now preserved in the *_singlelayer.ipynb and *_multilayer.ipynb notebooks.

--- rcwa_tf/src/solver_metasurface.py
+++ rcwa_tf/src/solver_metasurface.py
@@ -18,7 +18,7 @@ def generate_copilot_metasurface_singlelayer(z, params):
 
     Args:
         z: A `tf.Tensor` of shape `(pixelsX, pixelsY)` specifying the relative permittivity
-        at each unit cell. Each entry in this tensor should be an float in [1,params['erd']].
+        at each unit cell. Each entry in this tensor should be a float in [1,params['erd']].
         
         params: A `dict` containing simulation and optimization settings.
 
@@ -77,7 +77,7 @@ def generate_copilot_metasurface_multilayer(z, device, params):
 
     Args:
         z: A `tf.Tensor` of shape `(pixelsX, pixelsY, 1)` specifying the relative permittivity
-        at each unit cell. Each entry in this tensor should be an float in [1,params['erd']]. This is
+        at each unit cell. Each entry in this tensor should be a float in [1,params['erd']]. This is
         the top layer of the design, which is currenlty being optimized.
         
         device: A `tf.Tensor` of shape `(batchsize, pixelsX, pixelsY, Nlay-1, Nx, Ny)` specifying the 
@@ -128,6 +128,64 @@ def generate_copilot_metasurface_multilayer(z, device, params):
     UR_t = tf.convert_to_tensor(UR, dtype = tf.float32)
     UR_t = tf.cast(UR_t, dtype = tf.complex64)
     
+    return ER_t, UR_t
+
+
+def generate_copilot_metasurface(h, params):
+    '''
+    Generates permittivity/permeability for a discrete, single-layer metasurface design.
+    The metasurface either has material or not at each grid position, and a substrate layer underneath.
+
+    Version 3: Uses a height representation of the surface, where h contains float heights
+    representing the material height at each metasurface pixel. A differentiable thresholding step limits
+    these heights and converts them to the stacked representation.
+
+    Args:
+        h: A `tf.Tensor` of shape `(pixelsX, pixelsY)` specifying the metasurface height
+        at each unit cell. Each entry in this tensor should be a float in [0,params['Nlay']-1].
+        
+        params: A `dict` containing simulation and optimization settings.
+
+    Returns:
+        ER_t: A `tf.Tensor` of shape `(batchSize, pixelsX, pixelsY, Nlayer, Nx, Ny)`
+        specifying the relative permittivity distribution of the unit cell.
+
+        UR_t: A `tf.Tensor` of shape `(batchSize, pixelsX, pixelsY, Nlayer, Nx, Ny)`
+        specifying the relative permeability distribution of the unit cell.
+    '''
+
+    # Retrieve simulation size parameters.
+    batchSize = params['batchSize']
+    pixelsX = params['pixelsX']
+    pixelsY = params['pixelsY']
+    Nlay = params['Nlay']
+    Nx = params['Nx']
+    Ny = params['Ny']
+
+    # Initialize relative permeability.
+    materials_shape = (batchSize, pixelsX, pixelsY, Nlay, Nx, Ny)
+    UR = params['urd'] * np.ones(materials_shape)
+    
+    # Limit optimization range.
+    h = tf.clip_by_value(h, clip_value_min = 0, clip_value_max = Nlay-1)
+    
+    # Convert height representation of the metasurface to stacked representation.
+    z = diff_height_to_stacked(h, params)
+    
+    # Repeat entries in z so that it has the shape (batchSize, pixelsX, pixelsY, 1, Nx, Ny)
+    z = z[tf.newaxis, :, :, :, tf.newaxis, tf.newaxis]
+    z = tf.tile(z, multiples = (batchSize, 1, 1, 1, Nx, Ny))
+
+    # Build substrate layer and concatenate along the layers dimension.
+    layer_shape = (batchSize, pixelsX, pixelsY, 1, Nx, Ny)
+    ER_substrate = params['ers'] * tf.ones(layer_shape, dtype = tf.float32)
+    ER_t = tf.concat(values = [z, ER_substrate], axis = 3)
+
+    # Cast to complex for subsequent calculations.
+    ER_t = tf.cast(ER_t, dtype = tf.complex64)
+    UR_t = tf.convert_to_tensor(UR, dtype = tf.float32)
+    UR_t = tf.cast(UR_t, dtype = tf.complex64)
+
     return ER_t, UR_t
 
 
@@ -245,6 +303,88 @@ def pixel_to_stacked(z, params, add_substrate=True, cast=True):
     return ER_t, UR_t
 
 
+def diff_threshold(x, coeff=1, thresh=1, offset=0, output_scaling=[0,1]):
+    '''
+    Performs a differentiable thresholding operation on the input, based on a sigmoid funciton.
+    
+    Args:
+        x: Float input to be thresholded. Can be a single number or a tensor of any dimension.
+        
+        coeff: Float coefficient determining steepness of thresholding.
+        
+        thresh: Float thresholding cutoff, i.e. where in x the step should occur.
+        
+        offset: Float minimum value assumed to occur in x. This value is subtracted from x
+            first before the operation is applied, such that the sigmoid cutoff occurs
+            halfway between in_offset and thresh.
+            
+        output_scaling: Float list of length 2 specifying limits to which output should
+            be renormalized.
+        
+        Both offsets should be < thresh, and coeff should be >= 0.
+    
+    Returns:
+        x: Thresholded input.
+    '''
+    
+    x_new = tf.math.sigmoid(coeff * (x - (offset + (thresh - offset)/2)) )
+    x_new = output_scaling[0] + (output_scaling[1] - output_scaling[0]) * x_new
+    return x_new
+
+
+def diff_height_to_stacked(h, params):
+    '''
+    Performs a differentiable transformation from the continuous height representation of a
+    metasurface pixel (i.e., a float representing the height of the surface at that pixel) to a
+    continous stacked representation (i.e., a list of floats representing relative permittivity of
+    the surface at each layer, at that pixel). Based on a differential thresholding using a
+    sigmoid function.
+    
+    Args:
+        h: Float height of the pixel. Can also be a tensor of any dimension.
+        
+        params: A `dict` containing simulation and optimization settings.
+    
+    Returns:
+        z: Stacked representation of the pixel.
+    '''
+    
+    #scaling = [params['eps_min'], params['eps_max']]
+    
+    #z = tf.stack([diff_threshold(h, thresh=3, coeff=params['sigmoid_coeff'], offset=2, output_scaling=scaling),
+    #              diff_threshold(h, thresh=2, coeff=params['sigmoid_coeff'], offset=1, output_scaling=scaling),
+    #              diff_threshold(h, thresh=1, coeff=params['sigmoid_coeff'], offset=0, output_scaling=scaling)],
+    #            )
+    
+    Nlay = params['Nlay']
+    z = tf.stack( [diff_threshold(h, thresh=Nlay-1-i,
+                                  coeff=params['sigmoid_coeff'],
+                                  offset=Nlay-2-i,
+                                  output_scaling = [params['eps_min'],params['eps_max']]) for i in range(Nlay-1) ] )
+
+    return tf.transpose(z, perm=[1,2,0])
+
+
+def evaluate_solution(focal_plane, params):
+    '''
+    Generates an evaluation score of a metasurface solution which can be used
+    to compare this solution to others.
+    
+    Args:
+        focal_plane: A `tf.Tensor` of shape `(batchSize, pixelsX * upsample, pixelsY * upsample)` describing 
+            electric field intensity on the focal plane.
+    
+        params: A `dict` containing simulation and optimization settings.
+    
+    Returns:
+        eval: Float evaluation score in range [0, inf).
+    '''
+    
+    r = params['focal_spot_radius']
+    index = (params['pixelsX'] * params['upsample']) // 2
+    return tf.math.reduce_sum(tf.abs(focal_plane[0, index-r:index+r, index-r:index+r])).numpy()
+
+        
 # Old versions
 
 '''
